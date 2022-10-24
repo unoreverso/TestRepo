f05e211339ed8ee021d85ed4bee75782d12b4679
unoreverso <ellistheboss@gmail.com>
2022-10-24 17:07:04 +0100

    first commit

---
+++ Test2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+
+namespace Test2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Directory.CreateDirectory("C:\\Test");
+            Console.WriteLine("Done");
+        }
+    }
+}
